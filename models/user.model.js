26832c92f5c091ae90ea10e6b1edf549c74358ac
Trooblet <ctv6348@autuni.ac.nz>
2019-09-19 22:01:46 +1200

    Added password function to User.model and added it to routing requests.

--- models/user.model.js
+++ models/user.model.js
@@ -2,16 +2,28 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+/**
+ * Creates a JSON representation to send to MongoDB
+ * Created by Ben Dagnin & James Hughes
+ * */
 const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      minlength: 3
-    }
-  },
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            minlength: 3
+        },
+
+        password: {
+            type: String,
+            required: true,
+            unique: false,
+            trim: true,
+            minlength: 8
+        }
+    },
   {
     timestamps: true
   }
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,24 @@
 const router = require("express").Router();
 let User = require("../models/user.model");
 
-router.route("/").get((req, res) => {
+/**
+ * Defines route to find a list of users
+ * Created by Ben Dagnin
+ */
+router.route("/users").get((req, res) => {
   User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json("Error: " + err));
 });
 
+/**
+ * Defines a route to add a user to the database
+ * Created by Ben Dagnin
+ */
 router.route("/add").post((req, res) => {
-  const username = req.body.username;
-  const newUser = new User({ username });
+    const username = req.body.username;
+    const password = req.body.username;
+  const newUser = new User({ username, password });
   newUser
     .save()
     .then(() => res.json("User added!"))
