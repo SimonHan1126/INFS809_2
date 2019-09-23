7e0382bd7378aaa1cfb810218980ca194dbcb65f
Trooblet <ctv6348@autuni.ac.nz>
2019-09-23 22:11:13 +1200

    Added repository routes

---
+++ models/repo.model.js
@@ -0,0 +1,25 @@
+/**
+ * Model class for the repository
+ * Created by James Hughes
+ * */
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const repoSchema = new Schema(
+    {
+
+        papername: { type: String, required: true },
+        pubdate: { type: Date, required: true },
+        journal: { type: String, required: true },
+        bibloref: { type: String, required: true },
+        authors: { type: String, required: true },
+        field: {type: String, required: true}
+
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Repo = mongoose.model("Repo", repoSchema);
+
+module.exports = Repo;
