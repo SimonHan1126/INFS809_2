4bc03b69f6fabe0e68a74299d41e4669dbee8272
nebsun <xnebsx@gmail.com>
2019-09-29 14:19:06 +1300

    attempt to link react app through express using a proxy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 
@@ -29,6 +30,16 @@ const usersRouter = require("./routes/users");
 
 app.use("/excercises", excercisesRouter);
 app.use("/users", usersRouter);
+
+declarationapp.use(express.static(path.join(__dirname, 'client/build')));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
 
 app.listen(port, () => {
   console.log(`server running on port: ${port}`);
