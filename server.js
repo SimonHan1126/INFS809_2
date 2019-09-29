0d9b26bffe364076498a9c553188f1b808966b2f
nebsun <xnebsx@gmail.com>
2019-09-29 14:42:56 +1300

    host react through express via proxy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 
@@ -13,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 //REMEMBER TO WHITELIST THE IP OF THE CONNECTING SERVER IN MONGODB
-const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI || "mongodb+srv://127.0.0.1";
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true
@@ -29,6 +30,16 @@ const usersRouter = require("./routes/users");
 
 app.use("/excercises", excercisesRouter);
 app.use("/users", usersRouter);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
 
 app.listen(port, () => {
   console.log(`server running on port: ${port}`);
