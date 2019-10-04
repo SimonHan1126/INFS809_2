71291ee2b69a4d3127b9612818396d5c82735ac1
nebsun <xnebsx@gmail.com>
2019-10-05 02:12:43 +1300

    added all bibtex fields

--- models/article.model.js
+++ models/article.model.js
@@ -7,18 +7,31 @@ const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema(
   {
-    title: { type: String, required: true },
+    address: { type: String, required: false },
+    annote: { type: String, required: false },
     author: { type: String, required: true },
-    date: { type: Date, required: false },
-    source: { type: String, required: false },
-    publisher: { type: String, required: false },
-    journal: { type: String, required: false },
-    volume: { type: String, required: false },
-    number: { type: String, required: false },
-    pagestart: { type: Number, required: false },
-    pageend: { type: Number, required: false },
+    booktitle: { type: String, required: false },
+    chapter: { type: String, required: false },
+    crossref: { type: String, required: false },
     doi: { type: String, required: false },
-    field: { type: String, required: false }
+    edition: { type: String, required: false },
+    editor: { type: String, required: false },
+    howpublished: { type: String, required: false },
+    institution: { type: String, required: false },
+    journal: { type: String, required: false },
+    key: { type: String, required: false },
+    month: { type: String, required: false },
+    note: { type: String, required: false },
+    number: { type: String, required: false },
+    organization: { type: String, required: false },
+    pages: { type: String, required: false },
+    publisher: { type: String, required: false },
+    school: { type: String, required: false },
+    series: { type: String, required: false },
+    title: { type: String, required: true },
+    type: { type: String, required: false },
+    volume: { type: String, required: false },
+    year: { type: String, required: false }
   },
 
   {
--- routes/articles.js
+++ routes/articles.js
@@ -12,28 +12,41 @@ const Article = require("../models/article.model");
  */
 router.post("/", (req, res) => {
   const article = new Article({
-    title: req.body.title,
+    address: req.body.address,
+    annote: req.body.annote,
     author: req.body.author,
-    date: req.body.date,
-    source: req.body.source,
-    publisher: req.body.publisher,
-    journal: req.body.journal,
-    volume: req.body.volume,
-    number: req.body.number,
-    pagestart: req.body.pagestart,
-    pageend: req.body.pageend,
+    booktitle: req.body.booktitle,
+    chapter: req.body.chapter,
+    crossref: req.body.crossref,
     doi: req.body.doi,
-    field: req.body.field
+    edition: req.body.edition,
+    editor: req.body.editor,
+    howpublished: req.body.howpublished,
+    institution: req.body.institution,
+    journal: req.body.journal,
+    key: req.body.key,
+    month: req.body.month,
+    note: req.body.note,
+    number: req.body.number,
+    organization: req.body.organization,
+    pages: req.body.pages,
+    publisher: req.body.publisher,
+    school: req.body.school,
+    series: req.body.series,
+    title: req.body.title,
+    type: req.body.type,
+    volume: req.body.volume,
+    year: req.body.year
   });
 
   console.log(article);
 
   article
     .save()
-    .then((data) => {
+    .then(data => {
       res.json(data);
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(400).json({ message: err });
     });
 });
@@ -44,10 +57,10 @@ router.post("/", (req, res) => {
  */
 router.get("/", (req, res) => {
   Article.find()
-    .then((data) => {
+    .then(data => {
       res.json(data);
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(400).json({ message: err });
     });
 });
@@ -58,10 +71,10 @@ router.get("/", (req, res) => {
  */
 router.get("/:id", (req, res) => {
   Article.findById(req.params.id)
-    .then((data) => {
+    .then(data => {
       res.json(data);
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(400).json({ message: err });
     });
 });
@@ -77,10 +90,10 @@ router.post("/search", (req, res) => {
    * MyModel.find({ name: /john/i }, 'name friends', function (err, docs) { })
    */
   Article.find(req.body)
-    .then((data) => {
+    .then(data => {
       res.json(data);
     })
-    .catch((err) => {
+    .catch(err => {
       res.status(400).json({ message: err });
     });
   //res.json(req.body);
