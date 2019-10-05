89d24ed16f1fd1f8f39e10237e66e710b5f3f1f1
nebsun <xnebsx@gmail.com>
2019-10-05 13:31:52 +1300

    path

--- routes/reactrouter.js
+++ routes/reactrouter.js
@@ -4,12 +4,12 @@ require("dotenv").config();
 
 if (process.env.NODE_ENV === "production") {
   router.use(express.static(path.join(__dirname, "../client/build")));
-  router.get("*", (req, res) => {
+  router.get("/", (req, res) => {
     res.sendfile(path.join((__dirname, "../client/build/index.html")));
   });
 }
 
-router.get("*", (req, res) => {
+router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
 
