828c0fd4982b07dfc43947b53624e52f2d75fa68
nebsun <xnebsx@gmail.com>
2019-10-05 22:29:28 +1300

    reordered model

--- models/article.model.js
+++ models/article.model.js
@@ -7,20 +7,20 @@ const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema(
   {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    doi: { type: String, required: false },
     address: { type: String, required: false },
     annote: { type: String, required: false },
-    author: { type: String, required: true },
     booktitle: { type: String, required: false },
     chapter: { type: String, required: false },
     crossref: { type: String, required: false },
-    doi: { type: String, required: false },
     edition: { type: String, required: false },
     editor: { type: String, required: false },
     howpublished: { type: String, required: false },
     institution: { type: String, required: false },
     journal: { type: String, required: false },
     key: { type: String, required: false },
-    month: { type: String, required: false },
     note: { type: String, required: false },
     number: { type: String, required: false },
     organization: { type: String, required: false },
@@ -28,10 +28,10 @@ const ArticleSchema = new Schema(
     publisher: { type: String, required: false },
     school: { type: String, required: false },
     series: { type: String, required: false },
-    title: { type: String, required: true },
     type: { type: String, required: false },
     volume: { type: String, required: false },
-    year: { type: String, required: false }
+    year: { type: String, required: false },
+    month: { type: String, required: false }
   },
 
   {
