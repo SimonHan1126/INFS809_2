4c7a72f26fc1c3efb482175f359c948f3dbecfb3
nebsun <xnebsx@gmail.com>
2019-10-12 23:40:05 +1300

    learning react

--- routes/reactrouter.js
+++ routes/reactrouter.js
@@ -9,6 +9,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+router.use(express.static(path.join(__dirname, "../client/build")));
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/public/index.html"));
 });
