52fe07865fe2a0f2838680621809672e32a2f071
Ben <nebsun@nebsun.com>
2019-10-17 14:28:33 +1300

    change route

--- routes/reactrouter.js
+++ routes/reactrouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = require("express").Router();
 const path = require("path");
 
-router.use(express.static(path.join(__dirname, "../client/build")));
+router.use(path.join(__dirname, "../client/build"));
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
